881dcafe629d7e9c19176381b7cbeb498bb6f35b
itissamdaniel <itissamdaniel@gmail.com>
2020-04-08 17:43:26 +0530

    InitialCommit

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostDelete(int Id)
+        {
+            var BookfromDb = await _db.Book.FindAsync(Id);
+            if (BookfromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Remove(BookfromDb);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
